b0377c84380e7650decf2f9e08e6e0c0e7bc890f
rare1k <hi@rare1k.dev>
2024-05-22 21:49:40 -0400

    feat: get shit working

---
+++ server/config.ts
@@ -0,0 +1,13 @@
+export default {
+    // Ratelimit config
+    ratelimit: {
+      duration: 300 * 1000,
+      max: 100,
+      responseMessage: "Global rate limit reached.",
+      skip: (req: any) => {
+        return !new URL(req.url).pathname.startsWith("/api");
+      },
+    },
+    // Webserver config
+    webserver: { port: 1667 },
+  };
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,15 @@
 import Elysia from "elysia";
+import { rateLimit } from "elysia-rate-limit";
+import staticPlugin from "@elysiajs/static";
 import logger from "../components/logger"
+import config from "./config"
+const c = require("colors/safe")
 const app:Elysia = new Elysia()
+app.use(rateLimit(config.ratelimit))
+app.use(staticPlugin({"assets":"server/static/","prefix":"/"}))
 
-app.get("/",()=>{return "k"})
+app.get("/api/",()=>{return "Alive!"})
 
-app.listen(1667,()=>{
-    logger.info("Starting server on port 1667")
+app.listen(config.webserver.port,()=>{
+    logger.info(`Starting server on port ${c.bold(config.webserver.port)}`)
 })
