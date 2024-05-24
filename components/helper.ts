8da0f3fae535a69a826b5beacfe14c73c6863f7b
rare1k <hi@rare1k.dev>
2024-05-24 19:29:03 -0400

    feat: add a check if the file doesnt exist

--- components/helper.ts
+++ components/helper.ts
@@ -1,6 +1,11 @@
 let ipr = require("ip-range-check")
+import logger from "../components/logger"
 
 export async function isVPN(ip:string): Promise<boolean> {
     let ranges = Bun.file("components/vpn_ips.txt")
+    if (!ranges.exists()) {
+        logger.error("vps_ips.txt does not exist! isVPN() will always return false.")
+        return false;
+    }
     return ipr(ip,(await ranges.text()).split("\n"))
 }
