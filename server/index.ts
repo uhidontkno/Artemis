76a071301f84bef089d00464680932fdeaadcae3
rare1k <hi@rare1k.dev>
2024-05-24 10:12:11 -0400

    feat: use some more middleware mostly for later

--- server/index.ts
+++ server/index.ts
@@ -3,10 +3,19 @@ import { rateLimit } from "elysia-rate-limit";
 import staticPlugin from "@elysiajs/static";
 import logger from "../components/logger"
 import config from "./config"
+
+import { nocache } from 'elysia-nocache';
+import { compression } from 'elysia-compression'
+import { ip } from "elysia-ip";
+
 const c = require("colors/safe")
 const app:Elysia = new Elysia()
+
 app.use(rateLimit(config.ratelimit))
 app.use(staticPlugin({"assets":"server/static/","prefix":"/"}))
+app.use(nocache)
+app.use(compression())
+app.use(ip())
 
 app.get("/api/",()=>{return "Alive!"})
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,6 @@ module.exports = {
   plugins: [require("@catppuccin/tailwindcss")({
     prefix: "",
     defaultFlavour: "mocha",
-  }),],
+  })],
 }
 
