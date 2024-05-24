0df0964a4bab47e4bd0a1ed943dc70c14b12778e
rare1k <hi@rare1k.dev>
2024-05-24 10:43:29 -0400

    feat: add status

--- bot/events/botReady.ts
+++ bot/events/botReady.ts
@@ -1,8 +1,21 @@
 import { createEvent } from "seyfert";
 import logger from "../../components/logger.ts"
+import { PresenceUpdateStatus } from "seyfert/lib/types/index";
+import { ActivityType } from "seyfert/lib/types/index";
 export default createEvent({
   data: { once: true, name: "botReady" },
   run(user, client) {
     logger.info(`${user.username}${(user.username == "Atermis") ? "" : " (Artemis instance)"} is ready`);
+    // @ts-ignore
+    client.gateway.setPresence({
+      activities: [{
+        name: "this server",
+        type: ActivityType.Watching,
+        state: `and ${client.guilds.list.length - 1} other server(s)!`,
+      }],
+      afk: false,
+      since: Date.now(),
+      status: PresenceUpdateStatus.DoNotDisturb,
+    })
   }
 })
