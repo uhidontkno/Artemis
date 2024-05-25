4332f77df641f9a128c4d1f1ed04aa30d141bd88
rare1k <hi@rare1k.dev>
2024-05-25 00:26:38 -0400

    feat: tweaks

--- server/static/verification.js
+++ server/static/verification.js
@@ -14,6 +14,7 @@ document.querySelector("h1").style.color = "#e78284"
 document.querySelector("h1").style.animation = "shake 0.82s cubic-bezier(.36,.07,.19,.97) both"
 },100)
 document.querySelector("h1").innerText = reason;
+document.title = "Verification Failed | Artemis"
 }
 
 function successVerif() {
@@ -29,17 +30,21 @@ s.querySelector("#moon").style.stroke = "#a6d189"
 document.querySelector("h1").style.color = "#a6d189"
 },100)
 document.querySelector("h1").innerText = "Success! You can now close this tab.";
+document.title = "Verification Successful | Artemis"
 }
 setTimeout(()=>{
     if (document.cookie.indexOf("43616368652E5665726966696564") != -1) {
-        failVerif("You already verified")
+        failVerif("You already verified");return
     }
     
     detectIncognito().then((result) => {
         if (result.isPrivate) {
             failVerif("Incognito Mode detected")
-        }
+        }; return
       });
+
+      successVerif()
+      document.cookie = `43616368652E5665726966696564=y+${Math.floor(Date.now()).toString(16)}; expires=${new Date(new Date().setFullYear(9999)).toUTCString()}; path=/`;
 },500)
     
 
