16b2bc130845c003bab1fd74a016990a1f0784db
rare1k <hi@rare1k.dev>
2024-05-24 22:05:06 -0400

    feat: start working on the verification

--- components/helper.ts
+++ components/helper.ts
@@ -1,5 +1,6 @@
 let ipr = require("ip-range-check")
 import logger from "../components/logger"
+import sqllite, { dbopen } from "../components/sqllite"
 
 export async function isVPN(ip:string): Promise<boolean> {
     let ranges = Bun.file("components/vpn_ips.txt")
@@ -8,4 +9,10 @@ export async function isVPN(ip:string): Promise<boolean> {
         return false;
     }
     return ipr(ip,(await ranges.text()).split("\n"))
+}
+
+export async function startVerification(id:number) {
+ let token = ((id+Date.now())*(8.44*Math.random())).toString(36).replaceAll(".","");
+ let db = dbopen("db.sql",true)
+ 
 }
