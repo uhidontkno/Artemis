7cc8cb3ea5a791b1e528f9367a14c44a5e6031c7
rare1k <hi@rare1k.dev>
2024-05-28 22:01:16 -0400

    feat: /setup command to setup a server for verification

--- bot/commands/setup.ts
+++ bot/commands/setup.ts
@@ -1,7 +1,9 @@
-import { Declare, Command, type CommandContext, Options, createRoleOption, Embed, ActionRow,Button } from 'seyfert';
+import { Declare, Command, type CommandContext, Options, createRoleOption, Embed, ActionRow,Button, ButtonInteraction } from 'seyfert';
 import { dbopen,dbwrite,dbread } from "../../components/sqllite"
 import { EmbedColors } from 'seyfert/lib/common';
-import { ButtonStyle } from 'seyfert/lib/types';
+import { ButtonStyle, MessageFlags } from 'seyfert/lib/types';
+
+
 @Declare({
   name: 'setup',
   description: 'Setup this server with Atermis.'
@@ -24,7 +26,36 @@ export default class SetupCommand extends Command {
         const cancel = new Button().setCustomId('cancel').setStyle(ButtonStyle.Secondary).setLabel('No');
         const row = new ActionRow<Button>().setComponents([cancel,confirm]);
         
-        await ctx.editOrReply({embeds:[em],components:[row]})
+        let m = await ctx.editOrReply({embeds:[em],components:[row]})
+        // @ts-expect-error
+        const collector = m.createComponentCollector();
+        collector.run('confirm', async (i:ButtonInteraction) => {
+            if (!i.isButton()) {return}
+            if (i.user.id != ctx.author.id) {
+                // @ts-expect-error
+                return i.write({content:"This is **not** your button.",flags:[MessageFlags.Ephemeral]})
+            }
+            let em = new Embed({title:"Success",color:EmbedColors.Green,description:"Set configuration for this server."})
+            // @ts-ignore
+            dbwrite(db,"config",(ctx.guildId || "-1"),btoa(JSON.stringify({"verifyRole":ctx.options.verifyRole})))
+            await ctx.editOrReply({embeds: [em]})
+        });
+        collector.run('cancel', async (i:ButtonInteraction) => {
+            if (!i.isButton()) {return}
+            if (i.user.id != ctx.author.id) {
+                // @ts-expect-error
+                return await i.write({content:"This is **not** your button.",flags:[MessageFlags.Ephemeral]})
+            }
+            // @ts-expect-error
+            await ctx.editOrReply({content:"Cancelled.",flags:[MessageFlags.Ephemeral]})
+        });
+        
+
+    } else {
+        let em = new Embed({title:"Success",color:EmbedColors.Green,description:"Set configuration for this server."})
+        // @ts-ignore
+        dbwrite(db,"config",(ctx.guildId || "-1"),btoa(JSON.stringify({"verifyRole":ctx.options.verifyRole})))
+        await ctx.editOrReply({embeds: [em]})
     }
   }
 }
