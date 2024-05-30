4704c6fbb1e8abf269b5ea3aaa75b5acf197e340
rare1k <hi@rare1k.dev>
2024-05-30 13:56:34 -0400

    feat: /verify checking and shit

--- bot/commands/verify.ts
+++ bot/commands/verify.ts
@@ -1,16 +1,39 @@
-import { Declare, Command, type CommandContext } from "seyfert";
-
+import { Declare, Command, type CommandContext, Embed,User,GuildMember } from "seyfert";
+import { EmbedColors } from "seyfert/lib/common";
+import { dbopen,dbread } from "../../components/sqllite";
 @Declare({
   name: "verify",
   description: "Verify yourself",
 })
 export default class VerifyCommand extends Command {
   async run(ctx: CommandContext) {
-    // @ts-ignore average latency between shards
-    const ping = ctx.client.gateway.latency;
-
-    await ctx.write({
-      content: `The ping is \`${ping}\``,
-    });
+    
+    await ctx.deferReply();
+    let db = dbopen("db.sql");
+    if (!dbread(db, "config", ctx.guildId || "-1")) {
+      let em = new Embed({
+        title: "Error",
+        color: EmbedColors.Red,
+        description:
+          "This server has not been setup yet. Please run the `/setup` command.",
+      });
+      await ctx.editOrReply({ embeds: [em] });
+      return;
+    }
+    
+    let roleId = JSON.parse(
+      // @ts-expect-error
+      atob(dbread(db, "config", ctx.guildId || "-1").value),
+    ).verifyrole;
+    let user = ctx.member
+    if (user.roles.keys.includes(roleId)) {
+      let em = new Embed({
+        title: "Error",
+        color: EmbedColors.Red,
+        description: "User already has the verified role.",
+      });
+      await ctx.editOrReply({ embeds: [em] });
+      return;
+    }
   }
 }
