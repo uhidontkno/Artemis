1f47bb11cac10ab2c90ef193568957cf1cd4c177
rare1k <hi@rare1k.dev>
2024-05-31 17:17:43 -0400

    feat: have both verif command and panel verify

---
+++ bot/handlers/verify.ts
@@ -0,0 +1,15 @@
+import { ComponentCommand, type ComponentContext } from 'seyfert';
+import VerifyCommand from '../commands/verify';
+import { MessageFlags } from 'seyfert/lib/types';
+
+export default class VerifyButton extends ComponentCommand {
+  componentType = 'Button' as const;
+
+  filter(ctx: ComponentContext<typeof this.componentType>) {
+    return ctx.customId === 'verify';
+  }
+
+  async run(ctx: ComponentContext<typeof this.componentType>) {
+    return await new VerifyCommand().run(ctx);
+  }
+}
