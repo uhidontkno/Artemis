59c0a255d95dd33dbce59f56578bd14ac2d3efbe
rare1k <hi@rare1k.dev>
2024-05-31 22:46:54 -0400

    chore: run prettier

--- bot/commands/privacy.ts
+++ bot/commands/privacy.ts
@@ -1,34 +1,45 @@
-import { Declare, Command, type CommandContext, Embed,Button,ActionRow } from "seyfert";
+import {
+  Declare,
+  Command,
+  type CommandContext,
+  Embed,
+  Button,
+  ActionRow,
+} from "seyfert";
 import { EmbedColors } from "seyfert/lib/common";
 import { dbopen, dbread } from "../../components/sqllite";
-import { MessageFlags,ButtonStyle } from "seyfert/lib/types";
+import { MessageFlags, ButtonStyle } from "seyfert/lib/types";
 @Declare({
   name: "privacy",
   description: "Show your data and our practices.",
 })
 export default class PrivacyCommand extends Command {
   async run(ctx: CommandContext) {
-    let db = dbopen("db.sql")
+    let db = dbopen("db.sql");
+    
+    let iph = dbread(db, "users", ctx.author.id)
     // @ts-ignore
-    let iph = dbread(db,"users",ctx.author.id) ? dbread(db,"users",ctx.author.id).value : "We don't have it."
-    let config = "{\"error\":\"You never setup your server with us!\"}"
+      ? dbread(db, "users", ctx.author.id).value
+      : "We don't have it.";
+    let config = '{"error":"You never setup your server with us!"}';
     try {
-    // @ts-ignore
-    config = atob(dbread(db,"config",ctx.guildId).value)
+      // @ts-ignore
+      config = atob(dbread(db, "config", ctx.guildId).value);
     } catch {}
     const del = new Button()
-    .setCustomId("delete")
-    .setStyle(ButtonStyle.Danger)
-    .setLabel("Delete my data!");
-  const row = new ActionRow<Button>().setComponents([del]);
+      .setCustomId("delete")
+      .setStyle(ButtonStyle.Danger)
+      .setLabel("Delete my data!");
+    const row = new ActionRow<Button>().setComponents([del]);
     await ctx.editOrReply({
-     embeds:[new Embed({
-        title:"Your data, and how we handle it.",
-        description: `
+      embeds: [
+        new Embed({
+          title: "Your data, and how we handle it.",
+          description: `
 **Your data that we have collected**:
 * Your IP address *hash* (note: we cant reverse this hash): \n\`${iph}\`
 * Your User ID: \`${ctx.author.id}\`
-${(ctx.guild()?.ownerId == ctx.author.id) ? `* Your server's configuration: \n\`\`\`json\n${config}\`\`\`` : ""}
+${ctx.guild()?.ownerId == ctx.author.id ? `* Your server's configuration: \n\`\`\`json\n${config}\`\`\`` : ""}
 
 **What data is collected**:
 Our verification bot is privacy respecting, so there is zero tracking, zero ads, and zero fingerprinting. [We are even open source under GPL-3.0](https://github.com/uhidontkno/Artemis). We only collect and store data that is absolutely necessary for the functioning of our verification bot. Here is the breakdown:
@@ -37,11 +48,11 @@ Our verification bot is privacy respecting, so there is zero tracking, zero ads,
 * **Your server configuration** (if you have set up your server with us) is a simple JSON containing all of the paramaters that you or a server admin entered when running the \`/setup\`. This is stored in Base64.
 * **Verification tokens** are generated based on randomness, time, and your Discord User ID converted to an alphanumerical string. These are temporarily stored in our database and are removed when you finish verifying (5 seconds after you visit our website). These are attached to your Discord User ID and determine if you are detected as an alt account or not.
 `,
-        color: EmbedColors.Aqua,
-        
-     })],
-     flags: MessageFlags.Ephemeral,
-     components: [row]
+          color: EmbedColors.Aqua,
+        }),
+      ],
+      flags: MessageFlags.Ephemeral,
+      components: [row],
     });
   }
 }
--- bot/handlers/deletemydata.ts
+++ bot/handlers/deletemydata.ts
@@ -1,8 +1,15 @@
-import { ComponentCommand, Embed, type ComponentContext,Button,ActionRow,ButtonInteraction } from "seyfert";
+import {
+  ComponentCommand,
+  Embed,
+  type ComponentContext,
+  Button,
+  ActionRow,
+  ButtonInteraction,
+} from "seyfert";
 import { MessageFlags } from "seyfert/lib/types";
 import { EmbedColors } from "seyfert/lib/common";
 import { ButtonStyle } from "seyfert/lib/types";
-import { dbread,dbdelete,dbopen } from "../../components/sqllite";
+import { dbread, dbdelete, dbopen } from "../../components/sqllite";
 
 export default class DeleteDataButton extends ComponentCommand {
   componentType = "Button" as const;
@@ -13,58 +20,87 @@ export default class DeleteDataButton extends ComponentCommand {
 
   async run(ctx: ComponentContext<typeof this.componentType>) {
     await ctx.deferReply(true);
-    if (!ctx.member?.permissions.has(ctx.member.permissions.Flags.Administrator)) {
-        let e = new Embed({title:"Insufficient Permissions",color:EmbedColors.Red,description:"You need the `Administrator` permission to delete your server's data off of Artemis. Keep in mind that this button **does not** delete your user data off of Artemis. "})
-        await ctx.editOrReply({ embeds: [e]});return 
-    }
-    let e = new Embed({title:"Are you 100% SURE you want to delete your server data?",color:EmbedColors.Red,description:"This is your **one and final** confirmation. Keep in mind that this button **DOES NOT** delete your user data (like your IP address) off of Artemis. All of your server data will be permamently lost with no recovery, and you will have to run `/setup` and re-invite me again. I will leave your server if you press Yes. Now, for the once and final time... Are you sure you want to delete your server data?"})
-    // buttons
-    const confirm = new Button()
-    .setCustomId("confirm")
-    .setStyle(ButtonStyle.Danger)
-    .setLabel("Yes");
-  const cancel = new Button()
-    .setCustomId("cancel")
-    .setStyle(ButtonStyle.Secondary)
-    .setLabel("No");
-  const row = new ActionRow<Button>().setComponents([cancel, confirm]);
-  
-  let m = await ctx.editOrReply({ embeds: [e], components: [row] });
-  // @ts-expect-error
-  const collector = m.createComponentCollector();
-  collector.run("cancel", async (i: ButtonInteraction) => {
-    i.editOrReply({content:"Operation cancelled. Thank you for keeping me around."})
-  });
-  collector.run("confirm", async (i: ButtonInteraction) => {
-    if (!i.isButton()) {
+    if (
+      !ctx.member?.permissions.has(ctx.member.permissions.Flags.Administrator)
+    ) {
+      let e = new Embed({
+        title: "Insufficient Permissions",
+        color: EmbedColors.Red,
+        description:
+          "You need the `Administrator` permission to delete your server's data off of Artemis. Keep in mind that this button **does not** delete your user data off of Artemis. ",
+      });
+      await ctx.editOrReply({ embeds: [e] });
       return;
     }
-    if (i.user.id != ctx.author.id) {
-      return i.editOrReply({
-        content: "This is **not** your button.",
+    let e = new Embed({
+      title: "Are you 100% SURE you want to delete your server data?",
+      color: EmbedColors.Red,
+      description:
+        "This is your **one and final** confirmation. Keep in mind that this button **DOES NOT** delete your user data (like your IP address) off of Artemis. All of your server data will be permamently lost with no recovery, and you will have to run `/setup` and re-invite me again. I will leave your server if you press Yes. Now, for the once and final time... Are you sure you want to delete your server data?",
+    });
+    // buttons
+    const confirm = new Button()
+      .setCustomId("confirm")
+      .setStyle(ButtonStyle.Danger)
+      .setLabel("Yes");
+    const cancel = new Button()
+      .setCustomId("cancel")
+      .setStyle(ButtonStyle.Secondary)
+      .setLabel("No");
+    const row = new ActionRow<Button>().setComponents([cancel, confirm]);
+
+    let m = await ctx.editOrReply({ embeds: [e], components: [row] });
+    // @ts-expect-error
+    const collector = m.createComponentCollector();
+    collector.run("cancel", async (i: ButtonInteraction) => {
+      i.editOrReply({
+        content: "Operation cancelled. Thank you for keeping me around.",
+      });
+    });
+    collector.run("confirm", async (i: ButtonInteraction) => {
+      if (!i.isButton()) {
+        return;
+      }
+      if (i.user.id != ctx.author.id) {
+        return i.editOrReply({
+          content: "This is **not** your button.",
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+      await i.deferReply(64);
+      i.editOrReply({
+        content: "Deleting your data...",
         flags: MessageFlags.Ephemeral,
       });
-    }
-    await i.deferReply(64);
-    i.editOrReply({content:"Deleting your data...",flags: MessageFlags.Ephemeral});
-    let db = dbopen("db.sql")
-    // @ts-expect-error
-    if (dbread(db,"config",ctx.guildId)) {
-        // @ts-expect-error
-        await ctx.client.messages.write(JSON.parse(atob(dbread(db,"config",ctx.guildId).value)).loggingchannel, {
+      let db = dbopen("db.sql");
+      // @ts-expect-error
+      if (dbread(db, "config", ctx.guildId)) {
+        
+        await ctx.client.messages.write(
+            // @ts-expect-error
+          JSON.parse(atob(dbread(db, "config", ctx.guildId).value))
+            .loggingchannel,
+          {
             content: `## <@${ctx.author.id}> HAS DELETED THIS SERVER'S DATA!\nSorry to see you all go @everyone... :sob:`,
-          });
-          await ctx.client.messages.write(ctx.channelId, {
-            content: `Sorry to see you all go... :sob:`,
-          });
+          },
+        );
+        await ctx.client.messages.write(ctx.channelId, {
+          content: `Sorry to see you all go... :sob:`,
+        });
         // @ts-expect-error
-        dbdelete(db,"config",ctx.guildId)
-        i.editOrReply({content:"Data deleted. Bye!",flags: MessageFlags.Ephemeral,});
+        dbdelete(db, "config", ctx.guildId);
+        i.editOrReply({
+          content: "Data deleted. Bye!",
+          flags: MessageFlags.Ephemeral,
+        });
         // @ts-expect-error
-        ctx.client.guilds.leave(ctx.guildId)
-    } else {
-        i.editOrReply({content:"What a miracle! There was no data to exist.",flags: MessageFlags.Ephemeral,});
-    }
-});
+        ctx.client.guilds.leave(ctx.guildId);
+      } else {
+        i.editOrReply({
+          content: "What a miracle! There was no data to exist.",
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+    });
   }
 }
