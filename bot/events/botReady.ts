24cafe126cee2cd337ea23126c4b7478a44bf542
rare1k <hi@rare1k.dev>
2024-06-01 09:42:09 -0400

    feat: add rotating statuses

--- bot/events/botReady.ts
+++ bot/events/botReady.ts
@@ -8,18 +8,44 @@ export default createEvent({
     logger.info(
       `${user.username}${user.username == "Artemis" ? "" : " (Artemis instance)"} is ready`,
     );
-    // @ts-ignore
-    client.gateway.setPresence({
-      activities: [
-        {
-          name: "this server",
-          type: ActivityType.Watching,
-          state: `and ${client.guilds.list.length - 1} other server(s) that support FOSS.`,
-        },
-      ],
-      afk: false,
-      since: Date.now(),
-      status: PresenceUpdateStatus.DoNotDisturb,
-    });
+    let statuses = [
+    {
+      name: "this server",
+      type: ActivityType.Watching,
+      state: `and ${client.guilds.list.length - 1} other server(s) that support FOSS.`,
+    },
+    {
+      name: "for verifications",
+      type: ActivityType.Watching,
+      state: ``,
+    },
+    {
+      name: "you",
+      type: ActivityType.Watching,
+      state: `and ${client.guilds.list.length - 1} other server(s) that support FOSS.`,
+    },
+    {
+      name: "out for alt accounts",
+      type: ActivityType.Watching,
+      state: ""
+    },
+    {
+      name: `🔗 ${process.env["DEPLOYMENT_URL"]}`,
+      type: ActivityType.Custom,
+      state: `🔗 ${process.env["DEPLOYMENT_URL"]}`
+    }
+  ]
+    setInterval(()=>{
+// @ts-ignore
+client.gateway.setPresence({
+  activities: [
+    statuses[Math.floor(Math.random() * statuses.length)]
+  ],
+  afk: false,
+  since: Date.now(),
+  status: PresenceUpdateStatus.DoNotDisturb,
+});
+    })
+    
   },
 });
